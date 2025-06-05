9a4e7b0da65a4c99a9041caa3ad3f41f9076b249
Abhishek Duharia <123815527+Abhishek442004@users.noreply.github.com>
2025-06-06 00:15:17 +0530

    Update Mlresponse.js

--- src/pages/Mlresponse.js
+++ src/pages/Mlresponse.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useRef } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://cbms-backend.onrender.com");
 
 const Mlresponse = () => {
   const [data, setData] = useState("Waiting for data...");
